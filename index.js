a9172867c784cf8fb7229aa765dd63b5f34d62c4
santiagoCeballos <santiago.bike95@gmail.com>
2021-01-11 10:31:37 -0500

    Final del curso GraphQl

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use stric'
 
 require('dotenv').config()
+const cors = require('cors')
 const LOG = require('debug')('app')
 const { join } = require('path');
 const { readFileSync } = require('fs');
@@ -11,18 +12,23 @@ const resolvers = require('./lib/resolves')
 // App init
 const app = require('express')();
 const port = process.env.port || 3000;
+// const isDev = process.env.NODE_ENV.trim() !== 'production'
+const isDev = true
 
 // Definiendo el schema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+// Middlewares
+app.use(cors())
+
 // Routes
 app.use('/api', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
   }), (req, res) => {
-    LOG('API')
+    res.send('QUE ONDA MI PEZ')
   }
 )
 
